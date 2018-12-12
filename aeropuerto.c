24cb0300c80b16b02bd4bad37275423b1439833c
david <dmartl02@estudiantes.unileon.es>
2018-12-12 16:43:07 +0100

    Librerías, constantes y alguna variable global

--- aeropuerto.c
+++ aeropuerto.c
@@ -1,1 +1,25 @@
 //PRACTICA FINAL DE SISTEMAS OPERATIVOS
+
+//LIBRERIAS
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
+#include <string.h>
+
+//CONSTANTES
+#define PASAJEROS 10
+#define PUESTOS 2
+
+//VARIABLES GLOBALES
+pthread_mutex_t semaforoLog;  //accesos al fichero de log
+pthread_mutex_t semaforoCola;  //accesos a la cola
+int contadorPasajeros;
+FILE *logFile;
+char *logFileName ="registroTiempos.log";
